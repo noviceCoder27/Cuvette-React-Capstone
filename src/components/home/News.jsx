175d3c8c8501cc9a69d1b23c7b2ca2ded68d0d56
mdGoswami <u19cse1001@cit.ac.in>
2023-10-27 13:40:25 +0530

    add fallback image in news

--- src/components/home/News.jsx
+++ src/components/home/News.jsx
@@ -1,12 +1,14 @@
 import { useEffect } from 'react'
 import styles from './News.module.css'
 import { useState } from 'react'
+import BackgroundImg from './../../assets/background.jpg' 
+
 
 const News = ({date,time}) => {
     const [news,setNews] = useState([]);
     useEffect(() => {
         const fetchNews = async () => {   
-            const response = await fetch('https://newsdata.io/api/1/news?apikey=pub_31565004ab0f1a64a071f0881c111e96252d6&q=pizza');
+            const response = await fetch('');
             const data = await response.json();
             setNews(data.results);
         }
@@ -15,10 +17,11 @@ const News = ({date,time}) => {
     return (
         <div className={styles.news}>
             <div className={styles["image-section"]}>
-                <img src= {news[0]?.image_url} alt="News Image" />
-                <p className={styles.heading}>Mountain</p>
+                {news[0]?.image_url && <img src= {news[0]?.image_url} alt="News Image" />}
+                {!news[0]?.image_url && <img src= {BackgroundImg} alt="Background" />}
+                <p className={styles.heading}>{news[0]?.title}</p>
                 <p className={styles.time}>{date.day}-{date.month}-{date.year} | {time.hours}:{time.minutes} {time.amOrPm}</p>
-                <div className={styles.overlay}></div>
+                <div className={styles.overlay}></div>  
             </div>
             <div className= {styles["news-content"]}>
                 <p>{news[0]?.content}</p>
